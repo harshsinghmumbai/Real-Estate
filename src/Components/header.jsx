7aa70a042ea86842b269a78790079dabb5f5d734
Harsh-Singh09 <harshrsingh552005@gmail.com>
2023-12-18 11:28:05 +0530

    style first component

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,16 +1,17 @@
 const Header = () => {
   return (
     <>
-      <header>
+      <header className="border-2 border-black flex justify-around">
         {/* -------------------logo of Website------------------ */}
         <img
+          className="w-44 h-11 cursor-pointer"
           src="https://funny-daffodil-350bc9.netlify.app/images/logo.png"
           alt="logo"
         />
 
         {/* -------------------Navigation------------------ */}
-        <nav>
-          <ul>
+        <nav className="border-2 border-black w-[30rem]">
+          <ul className="flex justify-between font-semibold font-sans text-lg mt-2">
             <li>Home</li>
             <li>About</li>
             <li>Service</li>
@@ -22,13 +23,15 @@ const Header = () => {
 
         {/* ---------------------------Signin Section---------------------------- */}
         <div className="">
-          <p>
-            <span>2</span>
+          <div className="border-2 border-black">
+            <p className="bg-green-500 rounded-full text-white w-3 h-3">
+              2
+            </p>
             <span>My List</span>
-          </p>
-          <button>
+          </div>
+          <button className="bg-green-500 p-1 rounded-lg">
             <span></span>
-            <span>Sign in</span>
+            <span className="text-white font-semibold">Sign In</span>
           </button>
         </div>
       </header>
