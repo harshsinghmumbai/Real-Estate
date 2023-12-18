8a60028ee0b859ddbedc6c15f2372ab5a036d27e
Harsh-Singh09 <harshrsingh552005@gmail.com>
2023-12-18 10:33:09 +0530

    format index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
+const App = () => {
+  return <>
+     
+  </>;
+};
 
- const App = () => {
-   return (
-     <>
-       <p className="text-lg font-semibold text-red-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus at excepturi, molestiae fugit nisi fuga ipsa nemo, voluptates velit dolorum qui sed amet facilis voluptas dolore! Quod voluptate odio molestiae eligendi! Molestiae blanditiis a quas. Excepturi molestiae explicabo, maiores doloremque quos ratione. Ratione, magni accusantium praesentium ad ullam nam voluptates.</p>
-     </>
-   )
- }
- 
- export default App
- 
+export default App;
---
+++ src/Components/header.jsx
@@ -0,0 +1,36 @@
+const header = () => {
+  return (
+    <>
+      <header>
+        {/* -------------------logo of Website------------------ */}
+        <img src="" alt="" />
+
+        {/* -------------------Navigation------------------ */}
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Service</li>
+            <li>Blog</li>
+            <li>Pricing</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+
+        {/* ---------------------------Signin Section---------------------------- */}
+        <div className="">
+          <p>
+            <span>2</span>
+            <span>My List</span>
+                  </p>
+                  <button>
+                      <span></span>
+                      <span>Sign in</span>
+                  </button>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default header;
