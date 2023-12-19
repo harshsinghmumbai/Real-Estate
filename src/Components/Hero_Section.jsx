7498bec3a8f25d5c2ac7fcbdafd0495202599309
Harsh-Singh09 <harshrsingh552005@gmail.com>
2023-12-19 20:31:55 +0530

    2 componet style completed

--- src/Components/Hero_Section.jsx
+++ src/Components/Hero_Section.jsx
@@ -1,3 +1,5 @@
+import { FaMagnifyingGlass } from "react-icons/fa6";
+
 const Hero_Section = () => {
   return (
     <>
@@ -17,23 +19,60 @@ const Hero_Section = () => {
               Find new & featured property located in your local city.
             </p>
 
-            <div className="bg-white w-[40rem] h-16">
-              <p className="flex flex-col">
+            <div className="bg-white w-[49.2rem] h-[6rem] mt-6 rounded-lg absolute top-[13.25rem] left-[-9.25rem] flex">
+              <p className="flex flex-col border-r-[1px] p-4 border-black h-24 w-52">
                 <label
                   htmlFor="City/Street"
-                  className="font-semibold text-gray-400"
+                  className="font-semibold text-gray-400 ml-1 mb-1"
                 >
                   City/Street
                 </label>
                 <input
-                  className="border-[1px] border-black rounded-md w-36 h-7"
+                  className="border-[1px] border-black rounded-md w-40 h-8 pl-2 text-black"
                   type="text"
                   name=""
                   id="City/Street"
                   placeholder="Location"
                 />
-                          </p>
-                          <hr />
+              </p>
+              <p className="flex flex-col border-r-[1px] p-4 border-black h-24 w-52">
+                <label
+                  htmlFor="Property"
+                  className="font-semibold text-gray-400 ml-1 mb-1"
+                >
+                  Property
+                </label>
+                <input
+                  className="border-[1px] border-black rounded-md w-40 h-8 pl-2 text-black"
+                  type="text"
+                  name=""
+                  id="Property"
+                  placeholder="Property Type"
+                />
+              </p>
+              <p className="flex flex-col border-r-[1px] p-4 border-black h-24 w-52">
+                <label
+                  htmlFor="Price Range"
+                  className="font-semibold text-gray-400 ml-1 mb-1"
+                >
+                  Price Range
+                </label>
+                <input
+                  className="border-[1px] border-black rounded-md w-40 h-8 pl-2 text-black"
+                  type="text"
+                  name=""
+                  id="Price Range"
+                  placeholder="Price"
+                />
+              </p>
+              <div className="flex">
+                <span className="text-lg font-semibold mt-5 ml-3 mr-3">
+                  Advance <br /> Filter
+                </span>
+                <button className="bg-green-500 text-white h-[3rem] w-[3.25rem] mt-6 rounded-lg pl-4 ">
+                  <FaMagnifyingGlass />
+                </button>
+              </div>
             </div>
           </div>
         </div>
