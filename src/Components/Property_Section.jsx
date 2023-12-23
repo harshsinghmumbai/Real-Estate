f9d0442401cb70c6a92dcb84ad3b856593d1820e
Harsh-Singh09 <harshrsingh552005@gmail.com>
2023-12-23 17:17:01 +0530

    50 % styleing is completed of this property_section components

--- src/Components/Property_Section.jsx
+++ src/Components/Property_Section.jsx
@@ -1,45 +1,51 @@
+import { FaHeart } from "react-icons/fa";
+import { FaLocationDot } from "react-icons/fa6";
+
 const Property_Section = () => {
   return (
     <>
-      <div className="">
-        <h1>Recent Property Listed</h1>
-        <p>
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-4xl font-bold tracking-wide inline-block mt-10 mb-2">
+          Recent Property Listed
+        </h1>
+        <p className="w-[29.5rem] text-lg text-center text-gray-500">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam
         </p>
-        <div className="">
-          <div className="w-[300px] rounded-md border">
-            <img
-              src="https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"
-              alt="Laptop"
-              className="h-[200px] w-full rounded-t-md object-cover"
-            />
-            <div className="p-4">
-              <h1 className="inline-flex items-center text-lg font-semibold">
-                About Macbook &nbsp; 
-              </h1>
-              <p className="mt-3 text-sm text-gray-600">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Excepturi, debitis?
-              </p>
-              <div className="mt-4">
-                <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
-                  #Macbook
-                </span>
-                <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
-                  #Apple
-                </span>
-                <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
-                  #Laptop
-                </span>
-              </div>
-              <button
-                type="button"
-                className="mt-4 w-full rounded-sm bg-black px-2 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-              >
-                Read
+      </div>
+
+      {/* --------------------------------Card Items--------------------------------- */}
+      <div className="p-20">
+        <div className="w-[300px] rounded-md border">
+          <img
+            src="https://images.unsplash.com/photo-1560448204-e02f11c3d0e2?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt="Laptop"
+            className="h-[200px] w-full rounded-t-md object-cover"
+          />
+          <div className="p-6">
+            <p className="flex justify-between">
+              <span className="bg-[#fff5e5] text-[#ff9800] p-1 rounded-md mb-1">
+                For Rent
+              </span>
+              <span className="text-xl text-gray-400 ">
+                <FaHeart />
+              </span>
+            </p>
+
+            <p className="text-lg font-semibold">Red Carpet Real Estate</p>
+            <p className="flex">
+              <span className="text-gray-500 pt-1 mr-2">
+                <FaLocationDot />
+              </span>
+              <span className="text-gray-500 mb-2">210 Zirak Road, Canada</span>
+            </p>
+            <hr />
+            <div className="flex justify-around mt-2">
+              <button className="bg-[#27ae60]  p-1 rounded-md font-semibold text-white">
+                $3700<span className="text-black">/sqft</span>
               </button>
+              <span>Apartment</span>
             </div>
           </div>
         </div>
