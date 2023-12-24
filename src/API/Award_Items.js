cc32af1fee3cd2aa2e23b63f7744c3c253e2d4d8
Harsh-Singh09 <harshrsingh552005@gmail.com>
2023-12-24 17:44:27 +0530

    completed Award_Items api 100%

--- src/API/Award_Items.js
+++ src/API/Award_Items.js
@@ -1,7 +1,33 @@
+import { IoBriefcase } from "react-icons/io5";
+import { FaTrophy } from "react-icons/fa";
+import { FaLightbulb } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
 
 const Award_Items = [
-    {
-        id: 1,
-        icon: 
-    }
-]
+  {
+    id: 1,
+    icon: <FaTrophy />,
+    follower: "32M",
+    description: "Blue Burmin Award",
+  },
+  {
+    id: 2,
+    icon: <IoBriefcase />,
+    follower: "43M",
+    description: "Mimo X11 Award",
+  },
+  {
+    id: 3,
+    icon: <FaLightbulb />,
+    follower: "51M",
+    description: "Australian UGC Award",
+  },
+  {
+    id: 4,
+    icon: <FaHeart />,
+    follower: "42M",
+    description: "IITCA Green Award",
+  },
+];
+
+export default Award_Items;
