b228864064d4141b3a21e06b12fd70150dfcb971
Harsh-Singh09 <harshrsingh552005@gmail.com>
2023-12-27 18:13:27 +0530

    compeleted 100% footer component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Agents_Section from "./Components/Agents_Section";
 import Award_Section from "./Components/Award_Section";
 import Explore_Section from "./Components/Explore_Section";
 import Feature_Section from "./Components/Feature_Section";
+import Footer from "./Components/Footer";
 import Hero_Section from "./Components/Hero_Section";
 import Package_Section from "./Components/Package_Section";
 import Property_Section from "./Components/Property_Section";
@@ -19,7 +20,8 @@ const App = () => {
       <Explore_Section />
       <Agents_Section />
       <Package_Section />
-      <Question_Component/>
+      <Question_Component />
+      <Footer/>
     </>
   );
 };
